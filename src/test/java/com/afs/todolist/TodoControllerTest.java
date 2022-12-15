09659faecb140775f45c7e1c2acf239ce863fac0
Marie Chow <chowma3@oocl.com>
2022-12-15 14:31:09 +0800

    test: add should_create_new_todo_when_perform_post_given_new_todo

--- src/test/java/com/afs/todolist/TodoControllerTest.java
+++ src/test/java/com/afs/todolist/TodoControllerTest.java
@@ -2,19 +2,23 @@ package com.afs.todolist;
 
 import com.afs.todolist.entity.Todo;
 import com.afs.todolist.repository.TodoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -44,7 +48,30 @@ public class TodoControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].text").value("test1"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].done").value(false));
     }
+    @Test
+    void should_create_new_todo_when_perform_post_given_new_todo() throws Exception {
+        //given
+        String id = new ObjectId().toString();
+        String text = "test1";
+        Boolean done = false;
+        Todo todo = new Todo(id, text, done);
+        String newTodoJson = new ObjectMapper().writeValueAsString(todo);
 
+        //when
+        client.perform(MockMvcRequestBuilders.post("/todos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newTodoJson))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isString())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.text").value(text))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.done").value(done));
+
+        // then
+        List<Todo> todos = todoRepository.findAll();
+        assertThat(todos, hasSize(1));
+        assertThat(todos.get(0).getText(), equalTo(text));
+        assertThat(todos.get(0).getDone(), equalTo(done));
+    }
     @Test
     void should_return_204_when_perform_delete_given_todos() throws Exception {
         //given
